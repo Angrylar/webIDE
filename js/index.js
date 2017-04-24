1b22b9b254e4e7344e3fcd33ff38530f25e91108
angrylar <renxiangyufeifei@163.com>
2017-04-24 11:02:48 +0800

    增加了对对象的翻译

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 window.onload = function () {
-
-var obj = {};
-  obj.a = {};
-    obj.a.q = "q12";
-    obj.a.w = "w12";
-    obj.a.e = "e12";
-    obj.a.r = "r12";
-    obj.a.w = {};
-      obj.a.w.x = "mmm"
-  obj.b = "123"
-var s = JSON.stringify(obj)
-var after = s.replace(/\{/g,"{<br/>")
-console.log(s)
-
+// var obj = {};
+//   obj.a = {};
+//     obj.a.q = "q12";
+//     obj.a.w = "w12";
+//     obj.a.e = "e12";
+//     obj.a.r = "r12";
+//     obj.a.w = {};
+//       obj.a.w.x = "mmm"
+//   obj.b = "123"
+// var s = JSON.stringify(obj)
+// var after = s.replace(/\{/g,"{<br/>")
+// console.log(s)
 
   var $div = document.getElementsByTagName('p')[0];
   var $getMsg = document.getElementsByClassName('getMsg')[0];
@@ -22,49 +20,8 @@ console.log(s)
 
   var tabmargin = '&nbsp;&nbsp;';
   $getMsg.onclick = function (){
-    // console.log($div.innerText.replace(/\s/g, ""))
-    // console.log(tabmargin.length)
     try {
       console.log(JSON.parse($div.innerText))
-                                              // var temp = '';
-                                              // var contentStringMin = $div.innerText.replace(/\{/g, "{<br/>");
-                                              //     contentStringMin = contentStringMin.replace(/\}/g,"<br/>}");
-                                              //     contentStringMin = contentStringMin.replace(/\[/g, "[<br/>");
-                                              //     contentStringMin = contentStringMin.replace(/\]/g,"<br/>]");
-                                              //     contentStringMin = contentStringMin.replace(/\,/g,",<br/>");
-                                              // // $after.innerHTML = contentStringMin;
-                                              // console.log(contentStringMin.split('{<br/>').length)
-                                              // for (let i = 0; i < contentStringMin.split('{<br/>').length; i++) {
-                                              //   if (i != contentStringMin.split('{<br/>').length-1) {
-                                              //     temp += contentStringMin.split('{<br/>')[i] + '{<br/>' +tabmargin;
-                                              //   } else {
-                                              //     temp += contentStringMin.split('{<br/>')[i];
-                                              //   }
-                                              //   tabmargin = tabmargin + '&nbsp;&nbsp;';
-                                              // }
-                                              // contentStringMin = temp;
-                                              // temp="";
-                                              // var tabmarginstring = "";
-                                              // var tabmarginArr = tabmargin.split(';');
-                                              // console.log(tabmarginArr)
-                                              // tabmarginArr.pop();
-                                              // tabmarginArr.pop();
-                                              // tabmarginArr.pop();
-                                              // tabmarginArr.pop();
-                                              // tabmarginArr.pop();
-                                              // for (let i = 0; i < contentStringMin.split('<br/>}').length; i++) {
-                                              //   console.log(1)
-                                              //   tabmarginstring = tabmarginArr.join(';');
-                                              //   temp += contentStringMin.split('<br/>}')[i] + '<br/>'+ tabmarginstring +'}';
-                                              //
-                                              //   tabmarginArr.pop();
-                                              //   tabmarginArr.pop();
-                                              // }
-                                              // console.log(temp);
-                                              // console.log(tabmarginArr)
-                                              //
-                                              //
-                                              // $after.innerHTML = temp;\\
       var getMsgChar = $div.innerText;
       var charArr = [];
 
@@ -94,6 +51,10 @@ console.log(s)
             charArr.push(str.charAt(i));
             charArr.push('<br/>');
             charArr.push(spaceFunc(counter));
+          } else if (str.charAt(i) == ':') {
+            charArr.push('&nbsp;')
+            charArr.push(str.charAt(i));
+            charArr.push('&nbsp;')
           } else {
             charArr.push(str.charAt(i));
           }
@@ -102,15 +63,12 @@ console.log(s)
 
       }
       $after.innerHTML = traversal(getMsgChar);
-
-
     } catch (e) {
       alert('请你特么输入正确的json格式');
       console.log(e);
     } finally {
       console.log('finally')
     }
-
   }
   var count = 1;
   var oldLineNum = 0;
